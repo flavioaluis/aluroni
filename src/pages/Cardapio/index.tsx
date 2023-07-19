e706f5403260f4c1f42f50c630729bcb43eaaa35
flavioaluis <flavio.aluis@gmail.com>
2023-07-19 17:21:17 -0300

    Creating filter

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo} from 'assets/logo.svg';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
+    const [filtro,setFiltro] = useState<number | null>(null);
 
     return ( 
     <main>
@@ -19,6 +21,9 @@ export default function Cardapio() {
             <Buscador 
                 busca={busca} 
                 setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+                    <Filtros  filtro={filtro} setFiltro={setFiltro}/>    
+                </div>
             </section>
     </main>)
 }
